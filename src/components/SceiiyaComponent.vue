11a6af2ad8dd7ceae125bee2a351fd0b18af13e5
sceiiya <meetscheidj@gmail.com>
2024-12-12 02:19:38 +0800

    first commit

---
+++ src/components/SceiiyaComponent.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  message: string
+  github: string
+  porfolio: string
+}>()
+</script>
+
+<template>
+  <section class="sceiiya">
+    <h1>Hi UNIVERSE!</h1>
+    <h2>This is {{ name }}</h2>
+    <div>
+      <p>{{ message }}</p>
+    </div>
+    <div>
+      <ol>
+        <li>
+          <a :href="github" target="_blank"> meet me at my github </a>
+        </li>
+        <li>
+          <a :href="porfolio" target="_blank"> meet me at my portfolio </a>
+        </li>
+      </ol>
+    </div>
+  </section>
+</template>
+
+<style>
+* {
+  color: lightgray;
+}
+
+h1 {
+  background-color: crimson;
+  font-size: 50px;
+}
+</style>
---
+++ src/views/SceiiyaView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import SceiiyaComponent from '@/components/SceiiyaComponent.vue'
+</script>
+
+<template>
+  <main>
+    <SceiiyaComponent
+      name="Sceiiya"
+      message="I'm searching for a specific cosmic child"
+      github="https://github.com"
+      porfolio="https://yesimscheidj.rocks"
+    />
+  </main>
+</template>
+
+<style>
+main {
+  width: 100%;
+  height: 100%;
+  background-color: azure;
+}
+</style>
